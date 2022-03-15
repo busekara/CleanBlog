605590b2aa6ddbe41b6f95703356d8fa8438f868
Buse Kara <buse.kara@arfitect.com>
2022-03-15 14:06:18 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
-app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
-});
-const port = 3000;
+app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı.`);
 });
